796597bc540baa5309aae5cf104f02f1e6bf8e78
Harley <harley@bigdoor.com>
2013-09-21 10:34:43 -0700

    Add serving of index.html

--- onehourinvestor/onehourinvestor.py
+++ onehourinvestor/onehourinvestor.py
@@ -1,11 +1,20 @@
+import os
+
 import webapp2
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
 
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.headers['Content-Type'] = 'text/html'
+
+        template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+        self.response.write(template.render({}))
 
 
 application = webapp2.WSGIApplication([
