bf5bd13780e695e428587622213b936460ec70c8
Harley <harley@bigdoor.com>
2013-09-21 12:31:10 -0700

    Add email handler

--- onehourinvestor/onehourinvestor.py
+++ onehourinvestor/onehourinvestor.py
@@ -1,7 +1,10 @@
+import logging
 import os
 
 import webapp2
 import jinja2
+
+from google.appengine.api import mail
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -17,6 +20,30 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render({}))
 
 
+class SignUpHelper(webapp2.RequestHandler):
+
+    def get(self):
+        self.redirect('/')
+
+    def post(self):
+        email_address = self.request.get('email')
+        if not mail.is_email_valid(email_address):
+            self.redirect('/')
+        else:
+            sender_address = "One Hour Investor <harley@onehourinvestor.co>"
+            subject = "%s is intested!" % email_address
+            body = ""
+
+            try:
+                mail.send_mail(sender_address, email_address, subject, body)
+            except:
+                logging.error("Failed to send interest email to %s"
+                              % email_address)
+
+            self.redirect('/')
+
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/i-am-interested', SignUpHelper)
 ], debug=True)
