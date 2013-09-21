079a61e4aaea9abac2abbe01c0ff7772acf60abd
pchiang <rukqoa@gmail.com>
2013-09-21 13:03:37 -0700

    Signup done

---
+++ onehourinvestor/js/index.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    $(".btn-signup").click(signup);
+};
+
+function signup() {
+    var emailAddress = $("#email-signup").val();
+    
+    $.post("/i-am-interested", {
+        email : emailAddress
+    }).done(function(data) {
+        alert("Data Loaded: " + data);
+    });
+    
+    $("#signup-prompt").html("Thanks for signing up!");
+    $("#email-signup").remove();
+    $(".btn-signup").remove();
+    $("#signup-form").html("We will notify you when the site is launched.");
+}
