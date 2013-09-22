74fe078f617d048a6299dabd5002f158607ba7b9
Harley <harley@bigdoor.com>
2013-09-21 17:18:38 -0700

    Add lesson and dashboard html

--- onehourinvestor/onehourinvestor.py
+++ onehourinvestor/onehourinvestor.py
@@ -4,7 +4,6 @@ import os
 import webapp2
 import jinja2
 
-from google.appengine.api import mail
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -20,31 +19,26 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(template.render({}))
 
 
-class SignUpHelper(webapp2.RequestHandler):
+class LessonHandler(webapp2.RequestHandler):
 
     def get(self):
-        self.redirect('/')
+        self.response.headers['Content-Type'] = 'text/html'
 
-    def post(self):
-        email_address = self.request.get('email')
-        if not mail.is_email_valid(email_address):
-            self.redirect('/')
-        else:
-            sender_address = 'Interest Bot <interestbot@onehourinvestorswsea.appspotmail.com>'
-            receiver_address = 'onehourinvestor@gmail.com'
-            subject = '%s is intested!' % email_address
-            body = 'Nothing interesting to say about this?'
+        template = JINJA_ENVIRONMENT.get_template('lesson.html')
+        self.response.write(template.render({}))
 
-            try:
-                mail.send_mail(sender_address, receiver_address, subject, body)
-            except:
-                logging.error('Failed to send interest email to %s'
-                              % email_address)
 
-            self.redirect('/')
+class DashboardHandler(webapp2.DashboardHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+
+        template = JINJA_ENVIRONMENT.get_template('dashboard.html')
+        self.response.write(template.render({}))
 
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/i-am-interested', SignUpHelper)
+    ('/user/jimmy/lesson/beginner', LessonHandler),
+    ('/user/jimmy', DashboardHandler)
 ], debug=True)
