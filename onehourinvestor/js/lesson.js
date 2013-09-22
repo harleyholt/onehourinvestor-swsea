3fbeb9dda44f61cca00116578011321906c70cc9
pchiang <rukqoa@gmail.com>
2013-09-22 10:53:51 -0700

    Fixed add points sign visibility

--- onehourinvestor/js/lesson.js
+++ onehourinvestor/js/lesson.js
@@ -9,7 +9,7 @@ window.onload = function() {
     $(".btn-submit").attr("disabled", "disabled");
     $(".status").html("$" + money);
 
-    $(".right").click(function(){
+    $(".right").click(function() {
         $(".right").hide();
     });
 };
@@ -41,42 +41,48 @@ function choose() {
 
 function answer() {
     if ($(".active .list-group-item.active").hasClass("correct")) {
-        $(".active .answer-space").html("Correct!");
+        $(".active .answer-space").html("<span class=\"alert alert-success\">Correct!</span>");
         money += 1000;
         $(".status").html("$" + money);
 
-        $(".money-add").html("+$1000");
-        $(".money-add").show();
-        $(".money-add").css({
+        $("#money-add").show();
+        $("#money-add").html("+$1000");
+        $("#money-add").css({
             "opacity" : "1",
-            "color" : "blue"
+            "color" : "blue",
+            "display" : "run-in"
         });
-        $(".money-add").animate({
+        $("#money-add").animate({
             top : "-=100px",
             opacity : 0
         }, "slow", function() {
-            $(".money-add").css("top", "80%");
+            $("#money-add").css("top", "80%");
         });
-        $(".money-add").hide();
     } else {
-        $(".active .answer-space").html("Wrong!");
+        var page = $(".item.active");
+        if (page.hasClass("page2")) {
+            $(".active .answer-space").html("<span class=\"alert alert-danger\">Incorrect! Banks are insured against loss and are generally considered safe.</span>");
+        } else if (page.hasClass("page3")) {
+            $(".active .answer-space").html("<span class=\"alert alert-danger\">Incorrect! An index fund is a mutual fund that tracks a stock index.</span>");
+        }
+
         money -= 100;
         $(".status").html("$" + money);
 
-        $(".money-add").html("-$100");
-        $(".money-add").show();
-        $(".money-add").css({
+        $("#money-add").html("-$100");
+        $("#money-add").css({
             "opacity" : "1",
-            "color" : "red"
+            "color" : "red",
+            "display" : "run-in"
         });
-        $(".money-add").animate({
+        $("#money-add").animate({
             top : "-=100px",
             opacity : 0
         }, "slow", function() {
-            $(".money-add").css("top", "80%");
+            $("#money-add").css("top", "80%");
         });
-        $(".money-add").hide();
     }
+
     $(".active .btn-submit").attr('disabled', 'disabled');
     $(".active .list-group-item").unbind("mousedown");
 
