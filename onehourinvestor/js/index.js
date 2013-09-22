bee46328c1e6a916974f3b39b078d91585616b0c
pchiang <rukqoa@gmail.com>
2013-09-21 20:43:29 -0700

    Updated lesson

--- onehourinvestor/js/index.js
+++ onehourinvestor/js/index.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     $(".btn-signup").click(signup);
+    $(".list-group-item").mousedown(choose);
+    $(".btn-submit").click(answer);
+    $(".btn-submit").attr("disabled", "disabled");
 };
 
 function signup() {
@@ -14,4 +17,27 @@ function signup() {
     $("#email-signup").remove();
     $(".btn-signup").remove();
     $("#signup-form").html("We will notify you when the site is launched.");
+}
+
+function choose() {
+    if ($(this).hasClass("active")) {
+        $(".active .list-group-item").removeClass("active");
+        $(".active .btn-submit").attr("disabled", "disabled");
+    } else {
+        $(".active .list-group-item").removeClass("active");
+        $(this).addClass("active");
+        $(".active .btn-submit").removeAttr("disabled");
+    }
+}
+
+function answer() {
+    if ($(".active.list-group-item.active").hasClass("correct")) {
+        $("#answer-space").html("Correct!");
+    } else {
+        $("#answer-space").html("Wrong!");
+    }
+    $(".active .btn-submit").attr('disabled', 'disabled');
+    $(".active .list-group-item").unbind("mousedown");
+    $(".percent").html("20%");
+    $(".progress-bar").width("20%");
 }
